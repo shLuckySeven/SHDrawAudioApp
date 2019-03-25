1add4cac315470c79b58fe5c16532eb8ee44ca32
gaoshuhuan <shuhuan.gao@mytogo.com>
2019-03-25 10:55:55 +0800

    修改controller文件，删除废弃代码

--- CWDemo/ViewController.m
+++ CWDemo/ViewController.m
@@ -16,12 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    id __weak obj =[NSMutableArray arrayWithObjects:@"1",@"2", nil];
-//    NSLog(@"%@",obj);
-//    [obj addObject:@"3"];
-//    NSLog(@"%@",obj);
-    self addObserver:<#(nonnull NSObject *)#> forKeyPath:<#(nonnull NSString *)#> options:<#(NSKeyValueObservingOptions)#> context:<#(nullable void *)#>
-    
 }
 //开始录制按钮点击事件
 - (IBAction)startRecordingBtnClick:(id)sender {
